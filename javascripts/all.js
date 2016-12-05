a54265b53911f8bf5c2179694a3368f4cd1d095b
Monique Marchwiany <monique@launchpadlab.com>
2016-12-05 16:37:44 -0600

    Site updated to 52323ea

--- javascripts/all.js
+++ javascripts/all.js
@@ -79,260 +79,72 @@ $(document).ready(function() {
   })
 });
 
-$("input").on('change', function(){
-  var form = $(this).closest('form'),
-     data = form.serialize(),
-     results = $("#results");
-
-  results.text(data);
+$("document").ready(function() {
+  $(".slider").rangeslider();
 });
-
-
-$(".step-by-step").each(function(){
-      var container = $(this),
-        numSteps = container.children('.step').length - 1,
-        ratio = 100 / numSteps ,
-        determinate = $("<div></div>")
-          .addClass('determinate')
-          .css('width', '0%'),
-        progress = $("<div></div>")
-          .addClass('progress')
-          .append( determinate );
-
-      for( i=1; i<=numSteps; i++){
-        var dot = $("<span></span>")
-          .addClass('dot')
-          .css('left', (ratio * i) + "%");
-        progress.append( dot );
-      }
-      progress.hide();
-
-      container.after( progress );
-    });
-    $(".step-by-step .btn").on('click', function(event){
-      event.stopPropagation();
-      event.preventDefault();
-
-      var container = $(this).closest('.step-by-step'),
-        numSteps = container.children('.step').length - 1,
-        current = $(this).closest('.step'),
-        index = current.index(),
-        next = current.next('.step'),
-        progress = container.siblings('.progress').first(),
-        determinate = progress.find('.determinate'),
-        require = $(this).data('require'),
-        currentWidth = current.width(),
-        marginLeft = current.css('margin-left').replace("px", "") - (currentWidth * (index + 1)) - (8 * (index + 1));
-
-      if( require !== undefined ){
-        if( $("#"+require).val().length == 0 ){
-          $("#holder-"+require).addClass('require-notice');
-          return;
-        }
-      }
-
-      progress.show();
-
-      if( next.length > 0 ){
-        var width = ((index + 1) / numSteps) * 100;
-
-        progress.children('.dot').removeClass('hit');
-
-        progress.children('.dot').eq(index).delay(300).queue(function(next) {
-          $(this).addClass('hit');
-          next();
-        });
-
-        determinate.css('width', width+'%')
-        container.css('margin-left', marginLeft + 'px');
-        display_clauses();
-      }
-    });
-
-    $(".dot").on('click', function(event){
-      var container = $(this).closest('form').children('.step-by-step').eq(0);
-         index = $(this).index(),
-         current = container.children('.step').eq(index-1),
-         button = current.find('.btn').eq(0);
-
-      button.trigger('click');
-    });
-
-$("#reset").on("click", function(e){
-  e.preventDefault();
-  $(".step-by-step").css('margin-left', 0);
-});
-
-$(".holder").on('click', function(e){
-  e.preventDefault();
-
-  var element = $("<input>"),
-     holder = $(this),
-     name = $(this).attr('id').replace('holder-',''),
-     input = $("#"+name),
-     container = $("#opty_recform_modal_container"),
-     modal = $("#opty_recform_modal"),
-     helper = $("#opty_recform_modal_helper"),
-     helper_text = $(this).data('helper'),
-     type = $(this).data('type');
-
-  if( type == 'select' ){
-    type = 'hidden';
-    var options = $(this).data('options').split('|'),
-       fake = $("<div></div>"),
-       list = $("<ul></ul>");
-
-    fake.addClass('fake-input').text('Pick a State');
-    list.appendTo(fake);
-
-    options.forEach(function(option){
-      var item = $("<li></li>");
-      item.text(option).addClass('select-option').appendTo(list);
-      item.on('click', function(){
-        console.log(option);
-        element.val(option).trigger('keyup');
-        modal.trigger('click');
-      });
-    })
-
-    fake.appendTo(container);
-  }
-  element.attr('type', type).attr('name', name).attr('autocomplete', 'off');
-  helper.text(helper_text);
-
-  element.appendTo(container);
-  element.on('keyup', function(){
-    holder.text($(this).val());
-    input.val($(this).val()).trigger('change');
-  });
-
-
-
-  modal.fadeIn('slow').queue( function(next){
-    element.focus();
-    next();
-  });
-  modal.on('click', function(em){
-    // if( em.target.is() ){
-    //  console.log(em.target.type);
-    // }
-    $(this).fadeOut('slow').queue( function(next){
-      element.remove();
-      if( fake !== undefined ){
-        fake.remove();
-      }
-      next();
-    });
-  }).children().on('focus click', function(){
-    return false;
-  });
-
-  $("#opty_recform_wrap .fa-check").unbind('click').on('click', function(){
-    modal.trigger('click');
-  });
-  $(document).unbind('click').on('keyup', function(e){
-    if (e.keyCode == 13 || e.keyCode == 27) {
-      modal.trigger('click');
-    }
-  });
-});
-
-var display_clauses = function (){
-  $(".clause").each(function(){
-    var dep = $(this).data('dependency').split('|'),
-       clause = $(this);
-
-    dep.forEach(function( d ){
-      if( $("#"+d).val().length > 0 ){
-        $("#info-"+d).text($("#"+d).val());
-        clause.show();
-        return false;
-      }
-      else {
-        clause.hide();
-      }
-    });
-  });
-}
-;
-var active = 2;
-/****************************************
-Hide the current form element,
-Make it inline to show at the top,
-Bring the next element in.
-*****************************************/
-function continueForm(input) {
-  //Store the parent object for timeout purposes
-  var item = $(input).parent();
-  console.log(item);
-  //Increase the container to make space for the rest of the form items
-  $('.container').css({
-    'height': '200vh'
-  });
-  //Scroll to the bottom of the page
-  $('html, body').animate({
-    scrollTop: window.innerHeight
-  }, 1000);
-
-  //Class changes to handle showing/hiding
-  item.removeClass('show');
-  item.addClass('hide');
-  //Adding 'top' converts the input into a relative element, to be viewed by the user if scrolling up
-  setTimeout(function() {
-    item.addClass('top');
-  }, 750);
-
-  $('.step:nth-child(' + active +')').removeClass('active')
-
-
-  //Check if there's another element
-  if (item.next().length > 0) {
-    active++;
-    $('.step:nth-child(' + active +')').addClass('active');
-    item.next().addClass('show');
-    setTimeout(function() {
-      item.next().find('input').focus()
-    }, 1100);
-  } else {
-    //Unbind the enter key from the inputs
-    $('.input input').unbind("enterKey");
-    //Wait for animations to finish
-    setTimeout(function() {
-      //Return to the initial height
-      $('.container').css({
-        'height': '100vh'
-      });
-      //Scroll back to the top
-      $('html, body').animate({
-        scrollTop: 0
-      }, 800);
-    }, 800);
-  }
+$.fn.rangeslider = function(options) {
+  var obj = this;
+  var defautValue = obj.attr("value");
+  obj.wrap("<span class='range-slider'></span>");
+  obj.after("<span class='slider-container'><span class='bar'><span></span></span><span class='bar-btn'><span class='bar-value'>$0</span></span></span>");
+  obj.attr("oninput", "updateSlider(this)");
+  updateSlider(this);
+  return obj;
 };
 
-/********************************
-Bind the enter key to each input
-*********************************/
-$('.input input').bind("enterKey", function(e) {
-  continueForm(this);
+function updateSlider(passObj) {
+  var obj = $(passObj);
+  var value = obj.val();
+  var min = obj.attr("min");
+  var max = obj.attr("max");
+  var range = Math.round(max - min);
+  var percentage = Math.round((value - min) * 100 / range);
+  var nextObj = obj.next();
+  nextObj.find("span.bar-btn").css("left", percentage + "%");
+  nextObj.find("span.bar > span").css("width", percentage + "%");
+  nextObj.find("span.bar-btn > span").text(value);
+};
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
 });
-$('.input input').keyup(function(e) {
-  if (e.keyCode == 13) {
-    $(this).trigger("enterKey");
-  }
+/*!
+ * animsition v4.0.2
+ * A simple and easy jQuery plugin for CSS animated page transitions.
+ * http://blivesta.github.io/animsition
+ * License : MIT
+ * Author : blivesta (http://blivesta.com/)
+ */
+
+!function(t){"use strict";"function"==typeof define&&define.amd?define(["jquery"],t):"object"==typeof exports?module.exports=t(require("jquery")):t(jQuery)}(function(t){"use strict";var n="animsition",i={init:function(a){a=t.extend({inClass:"fade-in",outClass:"fade-out",inDuration:1500,outDuration:800,linkElement:".animsition-link",loading:!0,loadingParentElement:"body",loadingClass:"animsition-loading",loadingInner:"",timeout:!1,timeoutCountdown:5e3,onLoadEvent:!0,browser:["animation-duration","-webkit-animation-duration"],overlay:!1,overlayClass:"animsition-overlay-slide",overlayParentElement:"body",transition:function(t){window.location.href=t}},a),i.settings={timer:!1,data:{inClass:"animsition-in-class",inDuration:"animsition-in-duration",outClass:"animsition-out-class",outDuration:"animsition-out-duration",overlay:"animsition-overlay"},events:{inStart:"animsition.inStart",inEnd:"animsition.inEnd",outStart:"animsition.outStart",outEnd:"animsition.outEnd"}};var o=i.supportCheck.call(this,a);if(!o&&a.browser.length>0&&(!o||!this.length))return"console"in window||(window.console={},window.console.log=function(t){return t}),this.length||console.log("Animsition: Element does not exist on page."),o||console.log("Animsition: Does not support this browser."),i.destroy.call(this);var e=i.optionCheck.call(this,a);return e&&t("."+a.overlayClass).length<=0&&i.addOverlay.call(this,a),a.loading&&t("."+a.loadingClass).length<=0&&i.addLoading.call(this,a),this.each(function(){var o=this,e=t(this),s=t(window),r=t(document),l=e.data(n);l||(a=t.extend({},a),e.data(n,{options:a}),a.timeout&&i.addTimer.call(o),a.onLoadEvent&&s.on("load."+n,function(){i.settings.timer&&clearTimeout(i.settings.timer),i["in"].call(o)}),s.on("pageshow."+n,function(t){t.originalEvent.persisted&&i["in"].call(o)}),s.on("unload."+n,function(){}),r.on("click."+n,a.linkElement,function(n){n.preventDefault();var a=t(this),e=a.attr("href");2===n.which||n.metaKey||n.shiftKey||-1!==navigator.platform.toUpperCase().indexOf("WIN")&&n.ctrlKey?window.open(e,"_blank"):i.out.call(o,a,e)}))})},addOverlay:function(n){t(n.overlayParentElement).prepend('<div class="'+n.overlayClass+'"></div>')},addLoading:function(n){t(n.loadingParentElement).append('<div class="'+n.loadingClass+'">'+n.loadingInner+"</div>")},removeLoading:function(){var i=t(this),a=i.data(n).options,o=t(a.loadingParentElement).children("."+a.loadingClass);o.fadeOut().remove()},addTimer:function(){var a=this,o=t(this),e=o.data(n).options;i.settings.timer=setTimeout(function(){i["in"].call(a),t(window).off("load."+n)},e.timeoutCountdown)},supportCheck:function(n){var i=t(this),a=n.browser,o=a.length,e=!1;0===o&&(e=!0);for(var s=0;o>s;s++)if("string"==typeof i.css(a[s])){e=!0;break}return e},optionCheck:function(n){var a,o=t(this);return a=n.overlay||o.data(i.settings.data.overlay)?!0:!1},animationCheck:function(i,a,o){var e=t(this),s=e.data(n).options,r=typeof i,l=!a&&"number"===r,d=a&&"string"===r&&i.length>0;return l||d?i=i:a&&o?i=s.inClass:!a&&o?i=s.inDuration:a&&!o?i=s.outClass:a||o||(i=s.outDuration),i},"in":function(){var a=this,o=t(this),e=o.data(n).options,s=o.data(i.settings.data.inDuration),r=o.data(i.settings.data.inClass),l=i.animationCheck.call(a,s,!1,!0),d=i.animationCheck.call(a,r,!0,!0),u=i.optionCheck.call(a,e),c=o.data(n).outClass;e.loading&&i.removeLoading.call(a),c&&o.removeClass(c),u?i.inOverlay.call(a,d,l):i.inDefault.call(a,d,l)},inDefault:function(n,a){var o=t(this);o.css({"animation-duration":a+"ms"}).addClass(n).trigger(i.settings.events.inStart).animateCallback(function(){o.removeClass(n).css({opacity:1}).trigger(i.settings.events.inEnd)})},inOverlay:function(a,o){var e=t(this),s=e.data(n).options;e.css({opacity:1}).trigger(i.settings.events.inStart),t(s.overlayParentElement).children("."+s.overlayClass).css({"animation-duration":o+"ms"}).addClass(a).animateCallback(function(){e.trigger(i.settings.events.inEnd)})},out:function(a,o){var e=this,s=t(this),r=s.data(n).options,l=a.data(i.settings.data.outClass),d=s.data(i.settings.data.outClass),u=a.data(i.settings.data.outDuration),c=s.data(i.settings.data.outDuration),m=l?l:d,g=u?u:c,f=i.animationCheck.call(e,m,!0,!1),v=i.animationCheck.call(e,g,!1,!1),h=i.optionCheck.call(e,r);s.data(n).outClass=f,h?i.outOverlay.call(e,f,v,o):i.outDefault.call(e,f,v,o)},outDefault:function(a,o,e){var s=t(this),r=s.data(n).options;s.css({"animation-duration":o+1+"ms"}).addClass(a).trigger(i.settings.events.outStart).animateCallback(function(){s.trigger(i.settings.events.outEnd),r.transition(e)})},outOverlay:function(a,o,e){var s=this,r=t(this),l=r.data(n).options,d=r.data(i.settings.data.inClass),u=i.animationCheck.call(s,d,!0,!0);t(l.overlayParentElement).children("."+l.overlayClass).css({"animation-duration":o+1+"ms"}).removeClass(u).addClass(a).trigger(i.settings.events.outStart).animateCallback(function(){r.trigger(i.settings.events.outEnd),l.transition(e)})},destroy:function(){return this.each(function(){var i=t(this);t(window).off("."+n),i.css({opacity:1}).removeData(n)})}};t.fn.animateCallback=function(n){var i="animationend webkitAnimationEnd";return this.each(function(){var a=t(this);a.on(i,function(){return a.off(i),n.call(this)})})},t.fn.animsition=function(a){return i[a]?i[a].apply(this,Array.prototype.slice.call(arguments,1)):"object"!=typeof a&&a?void t.error("Method "+a+" does not exist on jQuery."+n):i.init.apply(this,arguments)}});
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'flip-in-x-fr',
+    outClass: 'flip-out-x-fr',
+    inDuration: 1500,
+    outDuration: 1500,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
 });
 
-/**************************************
-Also continue the form on button press
-***************************************/
-$('.input button').click(function() {
-  continueForm(this);
-});
 
-/*************************************
-Focus the first text input by default
-**************************************/
-$('.input.show input')[0].focus();
 
 
 
